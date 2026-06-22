642eb06e4df2cb27d9adeaa702d9dca7117c7362
Gaurav Kumar <gk16122004@gmail.com>
2026-06-22 20:03:17 +0530

    Time: 0 ms (100%), Space: 9.1 MB (81.37%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i = 0;
+        int n = s.size();
+
+        while (i < n && s[i] == ' ')
+            i++;
+
+        int sign = 1;
+
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            if (s[i] == '-')
+                sign = -1;
+            i++;
+        }
+
+        long long num = 0;
+
+        while (i < n && isdigit(s[i])) {
+            num = num * 10 + (s[i] - '0');
+
+            if (sign * num >= INT_MAX)
+                return INT_MAX;
+
+            if (sign * num <= INT_MIN)
+                return INT_MIN;
+
+            i++;
+        }
+
+        return (int)(sign * num);
+    }
+};

710855c79c2986b5e96024722684121fd174f13c
Gaurav Kumar <gk16122004@gmail.com>
2026-07-03 11:07:56 +0530

    Time: 212 ms (71.93%), Space: 334.1 MB (65.21%) - LeetHub

---
+++ 3620-network-recovery-pathways/3620-network-recovery-pathways.cpp
@@ -0,0 +1,80 @@
+class Solution {
+public:
+    int findMaxPathScore(vector<vector<int>>& edges, vector<bool>& online, long long k) {
+        int n = online.size();
+
+        vector<vector<pair<int,int>>> graph(n);
+        vector<int> indegree(n, 0);
+
+        int maxCost = 0;
+        for (auto &e : edges) {
+            graph[e[0]].push_back({e[1], e[2]});
+            indegree[e[1]]++;
+            maxCost = max(maxCost, e[2]);
+        }
+
+        // Topological Sort
+        queue<int> q;
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0)
+                q.push(i);
+        }
+
+        vector<int> topo;
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            topo.push_back(u);
+
+            for (auto &it : graph[u]) {
+                if (--indegree[it.first] == 0)
+                    q.push(it.first);
+            }
+        }
+
+        auto can = [&](int limit) {
+            const long long INF = (1LL << 60);
+            vector<long long> dist(n, INF);
+            dist[0] = 0;
+
+            for (int u : topo) {
+                if (dist[u] == INF)
+                    continue;
+
+                if (u != 0 && u != n - 1 && !online[u])
+                    continue;
+
+                for (auto &[v, w] : graph[u]) {
+                    if (w < limit)
+                        continue;
+
+                    if (v != n - 1 && !online[v])
+                        continue;
+
+                    if (dist[u] + (long long)w < dist[v]) {
+                        dist[v] = dist[u] + (long long)w;
+                    }
+                }
+            }
+
+            return dist[n - 1] <= k;
+        };
+
+        int low = 0;
+        int high = maxCost;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (can(mid)) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
